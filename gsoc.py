84675418803604941299bfcc1a17a73729387659
djain454 <f20170935@pilani.bits-pilani.ac.in>
2019-09-14 22:19:25 +0530

    final

--- gsoc.py
+++ gsoc.py
@@ -3,20 +3,17 @@ from bs4 import BeautifulSoup
 from pandas import DataFrame
 
 t=input("Enter the Year for which you want to export the organization list( Note that this should be less than the current year): ")
-url = 'https://summerofcode.withgoogle.com/archive/'+str(t)+'/organizations/'
 
-r = requests.get(url)
+r = requests.get('https://summerofcode.withgoogle.com/archive/'+str(t)+'/organizations/') #URL of gsoc
 
 if r.status_code == 404:
-    print('Archive of this year is not available. Please run the file again with different year')
+    print('Archive of this year is not available. Please run the file again with valid year.')
     exit()
 
 soup = BeautifulSoup(r.content, 'html.parser')
-
 rows = soup.select('section div ul li')
 
 link_list = []
-
 for row in rows:
         abc = 'https://summerofcode.withgoogle.com' + row.select_one('a')['href']
         link_list.append(abc)
@@ -26,7 +23,6 @@ contactlink = []
 techlist = []
 slots = []
 ideas = []
-
 for org_url in link_list:
         lisat = []
         r = requests.get(org_url)
@@ -45,8 +41,6 @@ for org_url in link_list:
         slots.append(len(slot))
         idea = soup.select_one(".org__button-container md-button")['href']
         ideas.append(idea)
-
-
 
 table = {'Org' : OrgName , 'Technologies' : techlist , 'Slots' : slots , 'Ideas Page' : ideas , 'Contact' : contactlink}
 df = DataFrame(table)
